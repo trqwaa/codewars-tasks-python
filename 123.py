ad6fb63a9e4ec46fd3bdb9a00e06d6db33f2880c
unknown <kamnevtima69@gmail.com>
2025-10-07 00:14:20 +0200

    I did (chatGBT) folder and naw i can do GBT-tasks for Python learning

--- 123.py
+++
@@ -1,6 +0,0 @@
-def move_zeros(lst):
-    y = []
-    if 0 in lst:
-        lst.add(y) and 0[::-1].add(y)
-
-    return y
---
+++ chatGBT-Solutions-for-learn/(GBT)Word frequency dictionary.py
@@ -0,0 +1,16 @@
+#Description:
+#Write a function word_frequency(text) that takes a string text and returns a dictionary, where:
+
+#key is the word,
+
+#value is the number of times the word appears in the text.
+
+
+def word_frequency(text):
+    text = "apple banana apple orange banana apple"
+    result = {}
+    for x in text.split():
+        result[x] = result.get(x, 0) + 1
+
+    return result
+print(word_frequency("apple banana apple orange banana apple"))
