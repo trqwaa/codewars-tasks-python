0a7fab973708bc95b5175f2c9f1087bf14e846bf
unknown <kamnevtima69@gmail.com>
2025-10-10 19:26:45 +0200

    one from 8 kuy and one from 7kyu

---
+++ Codewars.pythone/Python 7kyu/(7kyu) Mumbling.py
@@ -0,0 +1,13 @@
+def accum(st):
+    result_list = []
+    for x, letter in enumerate(st, start=1):
+        y = letter.upper() + letter.lower() * (x-1)
+        result_list.append(y)
+    pass
+    result = '-'.join(result_list)
+    return result
+
+
+#для такого идиота это было сложновато
+
+# enumerate - это функция, которая перебирает элементы последовательности и одновременно даёт их индексы.
